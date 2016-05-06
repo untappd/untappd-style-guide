9c06d61519928056238c23b311e4bb486cb27905
Drew Hamlett <i@drewh.me>
2016-05-06 15:30:23 -0400

    scss lint, toggle code, example block

--- Gemfile
+++ Gemfile
@@ -1,9 +1,12 @@
 source 'https://rubygems.org'
+ruby '2.3.0'
 
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
-gem 'redcarpet'
+gem 'kramdown'
 gem 'middleman-sprockets', '~> 4.0.0.rc.1'
 gem 'middleman-syntax'
+
+gem 'scss_lint', require: false
--- config.rb
+++ config.rb
@@ -9,33 +9,31 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-set :markdown_engine, :redcarpet
-set :markdown, fenced_code_blocks: true, smartypants: true
+set :markdown_engine, :kramdown
 
 activate :syntax, line_numbers: true
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+require 'middleman-syntax'
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+helpers do
+  def example(&block)
+    content = capture_html(&block)
+    html = Middleman::Syntax::Highlighter.highlight(content, 'html')
 
-# General configuration
+    concat_content content
+    concat_content "<a href='#' class='toggle-code'>Toggle Code</a>\n \n"
+    concat_content html
+  end
+end
 
 activate :sprockets
 
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
-  set :debug_assets, true
 end
 
-# Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_css
+  activate :minify_javascript
 end
